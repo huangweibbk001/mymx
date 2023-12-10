cdc7621d6748489ad37a6c31787fa8d363420fcf
unknown <huangweibbk001@googlemail.com>
2023-12-10 20:35:16 +0800

    sdf

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 setuptools.setup(name='mymx',
-      version='0.0.2',
+      version='0.0.4',
       description='just test python pip module',
       url='https://github.com/huangweibbk001/mymx',
-      author='__token__',
-      author_email='pypi-AgEIcHlwaS5vcmcCJDNmNTgzNjcxLTY0ZjctNDZmMS1hMWQwLWU1ZmIzODYyNTVmZAACKlszLCIwYjI2MTgwMC05NGI0LTRhZTUtOGM0Yy0wMmI0OWZlOWE2OTUiXQAABiApfewp3c1tzyu1I5zPCyD49968wJsOujSXB4rK2YfxHQ',
+      author='huangweigang',
+      author_email='huangweibbk001@googlemail.com',
       license='MIT',
       packages=setuptools.find_packages(),
       zip_safe=False)
