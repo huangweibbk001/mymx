c8ca9df7bbdfede4891dae81c9b58382373a599c
unknown <huangweibbk001@googlemail.com>
2023-12-10 19:42:52 +0800

    sdfs

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='mymx',
+      version='0.0.2',
+      description='just test python pip module',
+      url='github项目地址 https://github.com/pergrand/deeplearntool',
+      author='huangweigang',
+      author_email='huangweibbk001@googlemail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
